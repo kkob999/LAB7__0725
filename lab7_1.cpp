a31e0412d45c724daa7ba02360fc1a64e6a19128
kob555 <chom.kappa@gmail.com>
2020-12-30 01:06:57 +0700

    Finish

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
 using namespace std;
+string txt_s = "You have received Super Ultra Rare Unit!!!\n";
+string txt_a = "You have received 5 gems.\n";
+string txt_b = "You have received 1 gems.\n";
+string txt_c = "You have received 2000 coins.\n";
+string txt_d = "You have received very KAK items.\n";
 
 int main()
 {
   char rank;
   cout << "Input your rank: ";
   cin >> rank;
-  switch(rank){
-    case 'S': cout << "You have received Super Ultra Rare Unit!!!\n";
-    case 'A': cout << "You have received 5 gems.\n";
-    case 'B': cout << "You have received 1 gems.\n";
-    case 'C': cout << "You have received 2000 coins.\n";
-    case 'D': cout << "You have received very KAK items.\n";
+  if (rank=='S'){
+    cout<<txt_s<<txt_a<<txt_b<<txt_c<<txt_d;
+  }
+  else if(rank=='A'){
+    cout << txt_a << txt_b << txt_c << txt_d;
+  }
+  else if(rank=='B'){
+    cout << txt_b << txt_c << txt_d;
+  }
+  else if(rank=='C'){
+    cout << txt_c << txt_d;
+  }
+  else if(rank=='D'){
+    cout << txt_d;
+  }
+  else{
+    cout << "";
   }
   return 0;
 }
