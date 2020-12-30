a4916683e48eada529fa83c6925f30f723ad03f6
kob555 <chom.kappa@gmail.com>
2020-12-30 12:34:40 +0700

    finish

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,11 +1,51 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your property: "
-"Your status = "
-"UNFRIEND"
-"FRIEND"
-"BEST FRIEND"
-"ONE-NIGHT-STAND"
-"MARRIED"
-*/
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int age,tall,pp;
+    string status;
+    cout << "Enter your age: ";
+    cin >> age;
+
+
+    if(age <= 20){
+        cout << "Enter your height: ";
+        cin >> tall;
+        if(tall >= 160){
+            if(tall >= 175){
+            cout << "Enter your property: ";
+            cin >> pp;
+            if(pp > 69000000){
+                status = "MARRIED";
+                }
+            else{
+                status = "ONE-NIGHT-STAND";
+                }
+            }
+            else if(tall < 175){
+            status = "FRIEND";
+            }
+        }
+        else{
+            status = "UNFRIEND";
+        }
+    }
+    else{
+        if(age < 30){
+            cout << "Enter your property: ";
+            cin >> pp;
+            if(pp > 10000000){
+                status = "BEST FRIEND";
+            }
+            else{
+                status = "UNFRIEND";
+            }
+        }
+        else{
+            status = "UNFRIEND";
+        }
+    }
+    cout << "Your status = "<<status;
+
+}
